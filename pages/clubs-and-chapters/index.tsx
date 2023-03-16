0b09f3f69d228e34fb6df9a985b0a98e45cba95c
Karamraj Singh Anand <92857243+Karamraj@users.noreply.github.com>
2023-03-17 01:13:57 +0530

    Updated Club & Chapters Page

--- pages/clubs-and-chapters/index.tsx
+++ pages/clubs-and-chapters/index.tsx
@@ -1,8 +1,49 @@
 
 export default function Home() {
     return (
-        <div>
+        <div className="grid grid-cols-4 gap-4">
+            {/* -- Club & Chapter 1 -- */}
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+            <h2 className="card-title">Club & Chapter 1 Name</h2>
+            <p>Club & Chapter Description Goes Here</p>
+            <div className="card-actions">
+            <label htmlFor="my-modal-3" className="btn btn-secondary">open modal</label>
+            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+            <div className="modal">
+            <div className="modal-box relative">
+            <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <h3 className="text-lg font-bold">Club & Chapter 1 Name</h3>
+            <p className="py-4">Club & Chapter Detailed Description Goes Here</p>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
 
+            {/* -- Club & Chapter 2 -- */}
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+            <h2 className="card-title">Club & Chapter 2 Name</h2>
+            <p>Club & Chapter Description Goes Here</p>
+            <div className="card-actions">
+            <label htmlFor="my-modal-4" className="btn btn-primary">open modal</label>
+            <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+            <label htmlFor="my-modal-4" className="modal cursor-pointer">
+            <label className="modal-box relative" htmlFor="">
+            <h3 className="text-lg font-bold">Club & Chapter 2 Name</h3>
+            <p className="py-4">Club & Chapter Detailed Description Goes Here</p>
+            </label>
+            </label>
+            </div>
+            </div>
+            </div>
         </div>
     )
 }
