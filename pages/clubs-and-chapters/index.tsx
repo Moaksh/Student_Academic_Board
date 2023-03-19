d071a2fd45bce70525527faa0df804f9f8f733c4
Karamraj Singh Anand <92857243+Karamraj@users.noreply.github.com>
2023-03-19 14:22:19 +0530

    updated

--- pages/clubs-and-chapters/index.tsx
+++ pages/clubs-and-chapters/index.tsx
@@ -11,14 +11,14 @@ export default function Home() {
                                 <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
                          </figure>
                         <div className="card-body items-center text-center">
-                                <h2 className="card-title">Association For Computing Machinery</h2>
+                                <h2 className="card-title">Association For Computing Machinery (ACM)</h2>
                                 <p>The Association for Computing Machinery is a US-based international learned society for computing.</p>
                                 <div className="card-actions">
                                         <label htmlFor="my-modal-1" className="btn btn-secondary">View More</label>
                                         <input type="checkbox" id="my-modal-1" className="modal-toggle" />
                                         <label htmlFor="my-modal-1" className="modal cursor-pointer">
                                                 <label className="modal-box relative" htmlFor="">
-                                                        <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+                                                        <h3 c   lassName="text-lg font-bold">Association For Computing Machinery (ACM)</h3>
                                                         <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
                                                 </label>
                                         </label>
@@ -32,15 +32,15 @@ export default function Home() {
                                 <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
                         </figure>
                             <div className="card-body items-center text-center">
-                                <h2 className="card-title">Club & Chapter 1 Name</h2>
-                                 <p>Club & Chapter Description Goes Here</p>
+                                <h2 className="card-title">CodeChef</h2>
+                                 <p>CodeChef is a student organization that helps students improve their programming skills through competitions and events like workshops, hackathons, and coding boot camps.</p>
                                  <div className="card-actions">
-                                         <label htmlFor="my-modal-2" className="btn btn-secondary">open modal</label>
+                                         <label htmlFor="my-modal-2" className="btn btn-secondary">View More</label>
                                          <input type="checkbox" id="my-modal-2" className="modal-toggle" />
                                          <label htmlFor="my-modal-2" className="modal cursor-pointer">
                                                  <label className="modal-box relative" htmlFor="">
-                                                         <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-                                                         <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                                         <h3 className="text-lg font-bold">CodeChef</h3>
+                                                         <p className="py-4">CodeChef is a student organization that is dedicated to enhancing students' programming skills. The organization achieves this goal by providing students with various opportunities to learn, practice, and compete. These opportunities include workshops, hackathons, and coding boot camps. During these events, students can connect with like-minded individuals, learn new techniques, and receive feedback on their work. Additionally, the organization offers online resources such as tutorials and practice problems to further support students' learning. CodeChef is committed to helping students develop the skills they need to succeed in the world of programming and technology.</p>
                                                  </label>
                                          </label>
                                 </div>
