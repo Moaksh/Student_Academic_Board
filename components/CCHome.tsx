d711db06578dc3ef3822de86aadd67934de01631
Moaksh <moakshkakar@gmail.com>
2023-03-19 13:45:01 +0530

    clubs and chapters design update

--- components/CCHome.tsx
+++ components/CCHome.tsx
@@ -3,25 +3,25 @@ const CcHome = () => {
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="carousel carousel-center rounded-box w-full">
-                    <div className="carousel-item w-1/2">
+                    <div className="carousel-item w-full">
                         <img src="https://images.unsplash.com/photo-1678960445719-a672e74d976e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80" className="w-full" />
                     </div>
-                    <div className="carousel-item w-1/2">
+                    <div className="carousel-item w-full">
                         <img src="https://images.unsplash.com/photo-1667627126192-c481f0767496?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" className="w-full" />
                     </div>
-                    <div className="carousel-item w-1/2">
+                    <div className="carousel-item w-full">
                         <img src="https://images.unsplash.com/photo-1668670975877-4e95bcb16145?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" className="w-full" />
                     </div>
-                    <div className="carousel-item w-1/2">
+                    <div className="carousel-item w-full">
                         <img src="https://images.unsplash.com/photo-1668544282220-a00da32f0e9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" className="w-full" />
                     </div>
-                    <div className="carousel-item w-1/2">
+                    <div className="carousel-item w-full">
                         <img src="https://images.unsplash.com/photo-1541792705-eb6d8659a134?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80" className="w-full" />
                     </div>
-                    <div className="carousel-item w-1/2">
+                    <div className="carousel-item w-full">
                         <img src="https://images.unsplash.com/photo-1667673552568-4fe74a017a49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" className="w-full" />
                     </div>
-                    <div className="carousel-item w-1/2">
+                    <div className="carousel-item w-full">
                         <img src="https://images.unsplash.com/photo-1600262486242-235e64167963?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" className="w-full" />
                     </div>
                 </div>
--- pages/clubs-and-chapters/index.tsx
+++ pages/clubs-and-chapters/index.tsx
@@ -1,50 +1,71 @@
 
 export default function Home() {
     return (
-        <div className="grid grid-cols-4 gap-4">
-            {/* -- Club & Chapter 1 -- */}
-            <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
-            </figure>
-            <div className="card-body items-center text-center">
-            <h2 className="card-title">Club & Chapter 1 Name</h2>
-            <p>Club & Chapter Description Goes Here</p>
-            <div className="card-actions">
-            <label htmlFor="my-modal-3" className="btn btn-secondary">open modal</label>
-            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-            <div className="modal">
-            <div className="modal-box relative">
-            <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-            <h3 className="text-lg font-bold">Club & Chapter 1 Name</h3>
-            <p className="py-4">Club & Chapter Detailed Description Goes Here</p>
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>
+        <div className="p-16">
+                <h1 className={"font-black text-7xl pb-6"}>Clubs and Chapters</h1>
+                <div className="grid grid-cols-3 gap-4">
+                    {/* -- Club & Chapter 1 -- */}
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure className="px-10 pt-10">
+                                <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                         </figure>
+                        <div className="card-body items-center text-center">
+                                <h2 className="card-title">Club & Chapter 1 Name</h2>
+                                <p>Club & Chapter Description Goes Here</p>
+                                <div className="card-actions">
+                                        <label htmlFor="my-modal-1" className="btn btn-secondary">open modal</label>
+                                        <input type="checkbox" id="my-modal-1" className="modal-toggle" />
+                                        <label htmlFor="my-modal-1" className="modal cursor-pointer">
+                                                <label className="modal-box relative" htmlFor="">
+                                                        <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+                                                        <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                                </label>
+                                        </label>
+                                </div>
+                        </div>
+                    </div>
 
-            {/* -- Club & Chapter 2 -- */}
-            <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
-            </figure>
-            <div className="card-body items-center text-center">
-            <h2 className="card-title">Club & Chapter 1 Name</h2>
-            <p>Club & Chapter Description Goes Here</p>
-            <div className="card-actions">
-            <label htmlFor="my-modal-3" className="btn btn-secondary">open modal</label>
-            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-            <div className="modal">
-            <div className="modal-box relative">
-            <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-            <h3 className="text-lg font-bold">Club & Chapter 1 Name</h3>
-            <p className="py-4">Club & Chapter Detailed Description Goes Here</p>
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>
+                    {/* -- Club & Chapter 2 -- */}
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure className="px-10 pt-10">
+                                <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                        </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">Club & Chapter 1 Name</h2>
+                                 <p>Club & Chapter Description Goes Here</p>
+                                 <div className="card-actions">
+                                         <label htmlFor="my-modal-2" className="btn btn-secondary">open modal</label>
+                                         <input type="checkbox" id="my-modal-2" className="modal-toggle" />
+                                         <label htmlFor="my-modal-2" className="modal cursor-pointer">
+                                                 <label className="modal-box relative" htmlFor="">
+                                                         <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+                                                         <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                                 </label>
+                                         </label>
+                                </div>
+                            </div>
+                    </div>
+                        {/* -- Club & Chapter 3 -- */}
+                        <div className="card w-96 bg-base-100 shadow-xl">
+                                <figure className="px-10 pt-10">
+                                        <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                                </figure>
+                                <div className="card-body items-center text-center">
+                                        <h2 className="card-title">Club & Chapter 1 Name</h2>
+                                        <p>Club & Chapter Description Goes Here</p>
+                                        <div className="card-actions">
+                                                <label htmlFor="my-modal-3" className="btn btn-secondary">open modal</label>
+                                                <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                                                <label htmlFor="my-modal-3" className="modal cursor-pointer">
+                                                        <label className="modal-box relative" htmlFor="">
+                                                                <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
+                                                                <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                                        </label>
+                                                </label>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
         </div>
     )
 }
