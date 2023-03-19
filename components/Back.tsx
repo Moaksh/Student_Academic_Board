ea6d280d7a985e3bbe9e01acc0419ba4c20d9cef
Moaksh <moakshkakar@gmail.com>
2023-03-19 13:22:00 +0530

    font-fix, background match

--- components/Back.tsx
+++ components/Back.tsx
@@ -2,7 +2,7 @@ const Back = () => {
     return (
         <div className={"container "}>
         <div >
-            <svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice" id="bg-wrap">
+            <svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice" id="bg-wrap" className={"brightness-50 "}>
                 <defs>
                     <radialGradient id="Gradient1" cx="50%" cy="50%" fx="0.441602%" fy="50%" r=".5"><animate attributeName="fx" dur="34s" values="0%;3%;0%" repeatCount="indefinite"></animate><stop offset="0%" stop-color="rgba(255, 0, 255, 1)"></stop><stop offset="100%" stop-color="rgba(255, 0, 255, 0)"></stop></radialGradient>
                     <radialGradient id="Gradient2" cx="50%" cy="50%" fx="2.68147%" fy="50%" r=".5"><animate attributeName="fx" dur="23.5s" values="0%;3%;0%" repeatCount="indefinite"></animate><stop offset="0%" stop-color="rgba(255, 255, 0, 1)"></stop><stop offset="100%" stop-color="rgba(255, 255, 0, 0)"></stop></radialGradient>
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Back from "@/components/Back";
+import Hero from "@/components/Hero";
 
 const Layout = ({ children }) => {
     return (
