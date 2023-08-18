73ad79973f630105211e41fe50520ef5e4cd529b
Moaksh <moakshkakar@gmail.com>
2023-08-19 00:07:38 +0530

    events page update

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -20,8 +20,8 @@ const Index = () => {
                         </div>
                         <div className=" lg:w-2/3 py-4 lg:py-0 lg:px-4 ">
                             <p className={"font-medium"}>September 22 @ 8:00 am - September 27  @ 5:00pm</p>
-                            <h1 className={"font-bold text-3xl"}>Global AI</h1>
-                        <p className={""}>The Global AI Summit 2024, with the theme "International Conference on Artificial Intelligence and Emerging Technology (AISUMMIT-2024)," will be hosted by Bennett University. The event is scheduled to take place from September 4th to 6th, 2024 at Bennett University. Researchers from all over the world who are interested in a wide range of areas, including Optimization, Artificial Intelligence, Computational Intelligence, Machine Learning, Evolutionary Computation, Cyber Security, Fuzzy Logic, Signal Processing Theory and methods, Blockchain, Cloud Computing, Fog Computing, Internet of Things, Wireless Sensor Networks, various disorders and diseases, and many others, are cordially invited to attend.</p>
+                            <h1 className={"font-bold text-3xl"}>Orientation Week</h1>
+                        <p className={""}>Orientation Week is a week-long event that takes place at the beginning of each semester. It is a week full of fun activities and events that help you get to know your fellow students and the campus. It is also a great opportunity to learn about the services and resources available to you as a student at the University of Toronto</p>
 
                         </div>
                         <div className=" ">
@@ -52,7 +52,7 @@ const Index = () => {
                             <div className=" lg:w-2/3 py-4 lg:py-0 lg:px-4 ">
                                 <p className={"font-medium"}>August 22 @ 8:00 am - August 27 @ 5:00pm (2024)</p>
                                 <h1 className={"font-bold text-3xl"}>Global AI</h1>
-                                <p className={""}>Orientation Week is a week-long event that takes place at the beginning of each semester. It is a week full of fun activities and events that help you get to know your fellow students and the campus. It is also a great opportunity to learn about the services and resources available to you as a student at the University of Toronto.</p>
+                                <p className={""}>The Global AI Summit 2024, with the theme "International Conference on Artificial Intelligence and Emerging Technology (AISUMMIT-2024)," will be hosted by Bennett University. The event is scheduled to take place from September 4th to 6th, 2024 at Bennett University. Researchers from all over the world who are interested in a wide range of areas, including Optimization, Artificial Intelligence, Computational Intelligence, Machine Learning, Evolutionary Computation, Cyber Security, Fuzzy Logic, Signal Processing Theory and methods, Blockchain, Cloud Computing, Fog Computing, Internet of Things, Wireless Sensor Networks, various disorders and diseases, and many others, are cordially invited to attend.</p>
 
                             </div>
                             <div className=" ">
