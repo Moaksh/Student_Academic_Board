9b0942f35fba95a06448a5b7966323e1ab74ec07
Moaksh <moakshkakar@gmail.com>
2023-08-18 11:43:59 +0530

    added commitiee

--- pages/clubs-and-committees/index.tsx
+++ pages/clubs-and-committees/index.tsx
@@ -82,46 +82,35 @@ const Clubs = ({ clubs }) => {
                     </div>
                     ) : null
                 ))}
-                {/*/!* -- Clubs & Chapter 2 -- *!/*/}
-                {/*<div className="card w-96 bg-base-100 shadow-xl">*/}
-                {/*    <figure className="px-10 pt-10">*/}
-                {/*            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />*/}
-                {/*    </figure>*/}
-                {/*        <div className="card-body items-center text-center">*/}
-                {/*            <h2 className="card-title">CodeChef</h2>*/}
-                {/*             <p>CodeChef is a student organization that helps students improve their programming skills through competitions and events like workshops, hackathons, and coding boot camps.</p>*/}
-                {/*             <div className="card-actions">*/}
-                {/*                     <label htmlFor="my-modal-2" className="btn btn-secondary">View More</label>*/}
-                {/*                     <input type="checkbox" id="my-modal-2" className="modal-toggle" />*/}
-                {/*                     <label htmlFor="my-modal-2" className="modal cursor-pointer">*/}
-                {/*                             <label className="modal-box relative" htmlFor="">*/}
-                {/*                                     <h3 className="text-lg font-bold">CodeChef</h3>*/}
-                {/*                                     <p className="py-4">CodeChef is a student organization that is dedicated to enhancing students' programming skills. The organization achieves this goal by providing students with various opportunities to learn, practice, and compete. These opportunities include workshops, hackathons, and coding boot camps. During these events, students can connect with like-minded individuals, learn new techniques, and receive feedback on their work. Additionally, the organization offers online resources such as tutorials and practice problems to further support students' learning. CodeChef is committed to helping students develop the skills they need to succeed in the world of programming and technology.</p>*/}
-                {/*                             </label>*/}
-                {/*                     </label>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*</div>*/}
-                {/*    /!* -- Clubs & Chapter 3 -- *!/*/}
-                {/*    <div className="card w-96 bg-base-100 shadow-xl">*/}
-                {/*            <figure className="px-10 pt-10">*/}
-                {/*                    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />*/}
-                {/*            </figure>*/}
-                {/*            <div className="card-body items-center text-center">*/}
-                {/*                    <h2 className="card-title">Clubs & Chapter 1 Name</h2>*/}
-                {/*                    <p>Clubs & Chapter Description Goes Here</p>*/}
-                {/*                    <div className="card-actions">*/}
-                {/*                            <label htmlFor="my-modal-3" className="btn btn-secondary">open modal</label>*/}
-                {/*                            <input type="checkbox" id="my-modal-3" className="modal-toggle" />*/}
-                {/*                            <label htmlFor="my-modal-3" className="modal cursor-pointer">*/}
-                {/*                                    <label className="modal-box relative" htmlFor="">*/}
-                {/*                                            <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>*/}
-                {/*                                            <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>*/}
-                {/*                                    </label>*/}
-                {/*                            </label>*/}
-                {/*                    </div>*/}
-                {/*            </div>*/}
-                {/*    </div>*/}
+            </div>
+            <h1 className={"font-bold text-5xl px-6 "}>Committees</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-6">
+                {clubs.map(club => (
+                    (club.type === "committee") ?(
+
+                        <div className="card  bg-base-100 shadow-xl">
+                            {/*<figure className="px-10 pt-10">*/}
+                            {/*    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />*/}
+                            {/*</figure>*/}
+                            <div className="card-body items-center text-center">
+                                <img src={club.logo} alt={club.name} className={"w-40 rounded-2xl "}/>
+                                <h2 className="card-title pb-1">{club.name}</h2>
+                                {/*<p className={"text-left"}>{club.mission}</p>*/}
+                                {/*<Link href={"/clubs-and-committees/"+club.name} className={"btn btn-outline"}>asd</Link>*/}
+                                <div className="card-actions">
+                                    <label htmlFor={club._id} className="btn btn-outline">View More</label>
+                                    <input type="checkbox" id={club._id} className="modal-toggle" />
+                                    <label htmlFor={club._id} className="modal cursor-pointer">
+                                        <label className="modal-box relative" htmlFor="">
+                                            <h3 className="text-lg font-bold">{club.name}</h3>
+                                            <p className="py-4 text-left">{club.description}</p>
+                                        </label>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    ) : null
+                ))}
             </div>
         </div>
     )
